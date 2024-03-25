53e98287654e89322c2429c9c536dfab693e962c
feverdreme <jeffreyhuang08@gmail.com>
2024-03-25 00:14:45 -0400

    Change parse_lex to return instead of print

--- lib/parse_lex.ml
+++ lib/parse_lex.ml
@@ -1,7 +1,6 @@
 let lex_and_parse input = 
   let lexbuf = Lexing.from_string input in
   try
-    let parsed = Parser.main Lexer.read lexbuf in
-    Ast.string_of_prog parsed |> print_endline
+    Parser.main Lexer.read lexbuf
   with
   | Parser.Error -> failwith "Parser Error"

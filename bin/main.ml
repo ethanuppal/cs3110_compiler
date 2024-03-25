15cfbf7c917ecb2a8da11a6249c9d000f5e5ebad
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2024-03-24 21:10:34 -0400

    add beginnings of tui

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,19 @@
-let () = print_endline "Hello, World!"
+open Cs3110_compiler
+
+let print_help prog =
+  let open Printf in
+  printf "%s\n" Meta.get.description;
+  printf "\n";
+  printf "Usage: %s [-h|-v]\n" prog
+
+let print_version () =
+  let open Printf in
+  printf "%s %s\n" Meta.get.name (Meta.Version.to_string Meta.get.version);
+  printf "\n";
+  printf "Written by: %s\n" (String.concat ", " Meta.get.authors)
+
+let () =
+  match Sys.argv |> Tui.parse with
+  | Help { prog } -> print_help prog
+  | Version { prog = _ } -> print_version ()
+  | Error { prog; msg } -> Printf.printf "error: %s. use %s -h\n" msg prog

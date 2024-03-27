c16ade7cabab003c469a98cae208c518a9c2ddfe
Yey007 <yeeeeeeey007@gmail.com>
2024-03-27 11:09:08 -0400

    Add pretty printers for operations, expressions, and statements

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,3 +1,4 @@
+open Util
 (* Future: https://v2.ocaml.org/manual/gadts-tutorial.html *)
 (* This is good for enforcing things at the type level. *)
 
@@ -25,11 +26,15 @@ type stmt =
 (** A program is a series of statements. *)
 type prog = stmt list
 
+let pp_of string_of fmt x = Format.fprintf fmt "%s" (string_of x)
+
 (** [string_of_op op] is the string representation of [op]. *)
 let string_of_op = function
   | Plus -> "+"
   | Minus -> "-"
   | Times -> "*"
+
+let op_pp = pp_of string_of_op
 
 (** [string_of_expr expr] is the string representation of [expr]. *)
 let rec string_of_expr = function
@@ -41,11 +46,15 @@ let rec string_of_expr = function
           [ string_of_expr lhs; string_of_op op; string_of_expr rhs ]
       ^ ")"
 
+let expr_pp = pp_of string_of_expr
+
 (** [string_of_stmt stmt] is the string representation of [stmt]. *)
 let string_of_stmt = function
   | Declaration (vname, e) ->
       String.concat " " [ "let"; vname; "="; string_of_expr e ]
   | Print e -> "print " ^ string_of_expr e
 
+let stmt_pp = pp_of string_of_stmt
+
 (** [string_of_prog prog] is the string representation of [prog]. *)
-let string_of_prog = Util.(List.map string_of_stmt >> String.concat "\n")
+let string_of_prog = List.map string_of_stmt >> String.concat "\n"

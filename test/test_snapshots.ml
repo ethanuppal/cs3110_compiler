51b6450e406eed0cdb2b1602f3661093c976b035
Yey007 <yeeeeeeey007@gmail.com>
2024-04-21 22:51:39 -0400

    bruh

--- test/test_snapshots.ml
+++ test/test_snapshots.ml
@@ -22,7 +22,7 @@ let ir_suite =
     let ir = Ir_gen.generate statements in
     let main_cfg = List.hd ir in
     let simulator = Simulator.make () in
-    Simulator.simulate simulator main_cfg;
+    Simulator.run simulator main_cfg;
     Simulator.output_of simulator
   in
   Snapshot.make_test_suite snapshots_root "ir" transform

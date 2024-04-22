02fcdd9d917cfd05b51b354323f75f19ce9e17c9
Yey007 <yeeeeeeey007@gmail.com>
2024-04-21 22:55:54 -0400

    bruh

--- lib/backend/asm_emit.ml
+++ lib/backend/asm_emit.ml
@@ -3,8 +3,9 @@ let emit_ir section =
   failwith "i hate reg alloc"
 
 let emit_bb section cfg bb =
-  let ir = Basic_block.to_list cfg in
-  let jumps = Cfg.edges_from bb in
+  let ir = Basic_block.to_list bb in
+  let jumps = Cfg.out_edges cfg bb in
+  ignore jumps;
   List.iter (emit_ir section) ir;
   failwith "emit jumps"
 

f6c10b1b742f84820063e6f851904726fda8c66f
Yey007 <yeeeeeeey007@gmail.com>
2024-04-21 21:09:39 -0400

    Clean up snapshot testing

--- test/snapshot.ml
+++ test/snapshot.ml
@@ -1,36 +1,33 @@
 open Alcotest
 open X86ISTMB
 
-type transform = string -> string -> string
+type transform = string -> string
 
-let make_test_suite ?(version = Meta.get.version) root transform =
+let make_test_suite root suite transform =
   let open Util in
-  let version_string = Meta.Version.to_string version in
-  let snapshot_folder =
-    Util.merge_paths [ Project_root.path; root; version_string ]
-  in
-  print_endline snapshot_folder;
-  let files = Sys.readdir snapshot_folder in
+  let snapshots_folder = Util.merge_paths [ Project_root.path; root; suite ] in
+  let files = Sys.readdir snapshots_folder in
   let snapshots =
     files |> Array.to_list
-    |> List.filter (fun file -> not (String.get file 0 = '.'))
-    |> List.map (String.split_on_char '.' >> List.hd)
+    |> List.map Filename.remove_extension
     |> List.sort_uniq String.compare
   in
   let snapshot_test (snapshot : string) () =
-    let input_path = Util.merge_paths [ snapshot_folder; snapshot ^ ".in" ] in
-    let output_path = Util.merge_paths [ snapshot_folder; snapshot ^ ".out" ] in
+    let input_path = Util.merge_paths [ snapshots_folder; snapshot ^ ".in" ] in
+    let output_path =
+      Util.merge_paths [ snapshots_folder; snapshot ^ ".out" ]
+    in
     let input = read_file input_path in
     let expected = read_file output_path in
     try
-      let actual = transform (snapshot ^ ".in") input in
+      let actual = transform input in
       (check string)
         "Using the given input transformer should yield matching output to the \
          expected."
         expected actual
     with Parse_lex.ParseError msg -> fail msg
   in
-  let suite_name = Printf.sprintf "test/snapshots/%s" version_string in
+  let suite_name = Util.merge_paths [ root; suite ] in
   let snapshot_tests =
     snapshots
     |> List.map (fun snapshot -> (snapshot, `Slow, snapshot_test snapshot))
--- test/snapshot.mli
+++ test/snapshot.mli
@@ -1,11 +1,9 @@
-(** Let [f] be of type [transform]. Then, [f path contents] is the result of
-    transforming the string [contents] (from the file at path [path] relative to
-    the version root, e.g., for version v1.0.4, relative to
-    [test/snapshots/v1.0.4]). *)
-type transform = string -> string -> string
+(** Let [f] be of type [transform]. Then, [f contents] is the result of
+    transforming the string [contents] (which will usually come from a snapshot
+    file) *)
+type transform = string -> string
 
-(** [make_test_suite name f] is a snapshot test suite named [name] for version
-    [Meta.get.version] using snapshot transformation [f] (see the documentation
-    for [transform]). Optionally, the version can be manually specified. *)
-val make_test_suite :
-  ?version:X86ISTMB.Meta.Version.t -> string -> transform -> unit Alcotest.test
+(** [make_test_suite root suite f] is a snapshot test suit from snapshots in
+    [root/suite] using snapshot transformation [f] (see the documentation for
+    [transform]). *)
+val make_test_suite : string -> string -> transform -> unit Alcotest.test

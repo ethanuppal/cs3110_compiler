5cde1270bfeaf207f440b109fc5bf5a7f318bf11
Yey007 <yeeeeeeey007@gmail.com>
2024-05-15 15:53:48 -0400

    Document variable

--- lib/ir/variable.mli
+++ lib/ir/variable.mli
@@ -10,7 +10,15 @@ val id_of : t -> Id.t
 (** [to_string var] is [var] as a string. *)
 val to_string : t -> string
 
+(** [pp fmt var] pretty prints [var] to [fmt]. *)
 val pp : Format.formatter -> t -> unit
+
+(** Provides an arbitrary ordering of variables. *)
 val compare : t -> t -> int
+
+(** [equal var1 var2] is true when [id_of var1 = id_of var2], and false
+    otherwise. *)
 val equal : t -> t -> bool
+
+(** [hash var] is the hashcode of [var]. *)
 val hash : t -> int
